ad8939fb619b393dc0103eb155dc5b9ef4acff2c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-17 10:23:01 -0700

    Move server URL to .env file for flexibility

--- utils/server-utils.js
+++ utils/server-utils.js
@@ -1,9 +1,7 @@
 const request = require('superagent');
 
-const URL = 'https://minion-server.herokuapp.com';
-
 async function getAllResponses() {
-	const response = await request.get(`${URL}/responses`);
+	const response = await request.get(`${process.env.SERVER_URL}/responses`);
 
 	return response.body;
 }
