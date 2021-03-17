1a7a9da57f8bbda66294c9a7542c81f336ff5396
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-17 10:42:20 -0700

    Test deploy

--- utils/server-utils.js
+++ utils/server-utils.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const request = require('superagent');
 
 async function getAllResponses() {
+	console.log('getting all responses...');
 	const response = await request.get(`${process.env.SERVER_URL}/responses`);
-
+	console.log('responses gotten!');
 	return response.body;
 }
 
